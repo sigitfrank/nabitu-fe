97429108e9ba5cdde6a42fd0afa0920f8f630a51
sigit tunggul <sigittt53@gmail.com>
2025-02-06 19:12:30 +0700

    chore: setup project structure and packages

---
+++ src/app/invoices/add/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddInvoicePage = () => {
+  return <div>AddInvoicePage</div>;
+};
+
+export default AddInvoicePage;
---
+++ src/app/invoices/list/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListInvoicePage = () => {
+  return <div>ListInvoicePage</div>;
+};
+
+export default ListInvoicePage;
